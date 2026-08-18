18b7e7097e5b6e2e74abccd103352a1284c4caac
Jaden <jadentmahachi@gmail.com>
2026-08-18 09:46:44 +0200

    Update TMDB_KEY in config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,5 +1,5 @@
 var CONFIG = {
-  TMDB_KEY: '3a73619bbb8fc6d47742d1b5b2b707b5',
+  TMDB_KEY: '52a3fc8004d2ba2458a47686229ed1b3',
   TMDB_IMAGE_BASE: 'https://image.tmdb.org/t/p/',
   POSTER_SM: 'w342',
   POSTER_MD: 'w500',
